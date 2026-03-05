1fee29b5a875e2fa3dbbbececf8a1a77a67ef910
Charuhasen Kumaraswamy <charuhasen@gmail.com>
2026-03-05 15:12:37 +0000

    Remove Vercel Analytics and Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Analytics } from "@vercel/analytics/next";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 
 const geistSans = Geist({
@@ -110,8 +108,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Analytics />
-        <SpeedInsights />
+
       </body>
     </html>
   );
